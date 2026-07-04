3fc7e7a867a1f1808d64599a07700d0cd3ef177f
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-04 15:56:26 +1000

    destroy a task

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   post "tasks", to: "tasks#create"
   get "tasks/:id/edit", to: "tasks#edit", as: :edit
   patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
   get "tasks/:id", to: "tasks#show", as: :task
 end
